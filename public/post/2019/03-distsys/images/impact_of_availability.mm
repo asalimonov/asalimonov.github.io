58cc24042f9e8c0b8d8fd3e80a285213382e39ef
Aleksandr Salimonov <asalimonov@yandex-team.ru>
2019-12-15 06:40:11 +0300

    Migration to Hugo

---
+++ public/post/2019/03-distsys/images/impact_of_availability.mm
@@ -0,0 +1,16 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1547996568749" ID="ID_1221050671" MODIFIED="1547996888345" STYLE="bubble" TEXT="&#x417;&#x430;&#x43a;&#x430;&#x437;&#x447;&#x438;&#x43a;">
+<node CREATED="1547996735786" ID="ID_1876262040" MODIFIED="1547996888345" POSITION="right" STYLE="bubble" TEXT="&#x41f;&#x43e;&#x442;&#x435;&#x43d;&#x446;&#x438;&#x430;&#x43b;&#x44c;&#x43d;&#x430;&#x44f; &#x432;&#x44b;&#x440;&#x443;&#x447;&#x43a;&#x430;">
+<node CREATED="1547996819164" ID="ID_147250503" MODIFIED="1547996860865" STYLE="bubble" TEXT="&#x414;&#x43e;&#x441;&#x442;&#x443;&#x43f;&#x43d;&#x43e;&#x441;&#x442;&#x44c;">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node CREATED="1547996752587" ID="ID_1422123031" MODIFIED="1547996888345" POSITION="right" STYLE="bubble" TEXT="&#x426;&#x435;&#x43d;&#x430; &#x432;&#x43b;&#x430;&#x434;&#x435;&#x43d;&#x438;&#x44f; (TCO)">
+<node CREATED="1547996771488" ID="ID_1867072680" MODIFIED="1547996888345" STYLE="bubble" TEXT="&#x41a;&#x430;&#x43f;&#x438;&#x442;&#x430;&#x43b;&#x44c;&#x43d;&#x44b;&#x435; &#x437;&#x430;&#x442;&#x440;&#x430;&#x442;&#x44b;"/>
+<node CREATED="1547996785205" ID="ID_1975252341" MODIFIED="1547997401926" STYLE="bubble" TEXT="&#x41e;&#x43f;&#x435;&#x440;&#x430;&#x446;&#x438;&#x43e;&#x43d;&#x43d;&#x44b;&#x435; &#x437;&#x430;&#x442;&#x440;&#x430;&#x442;&#x44b;">
+<node CREATED="1547996796363" ID="ID_1988345565" MODIFIED="1547996905746" STYLE="bubble" TEXT="&#x414;&#x43e;&#x441;&#x442;&#x443;&#x43f;&#x43d;&#x43e;&#x441;&#x442;&#x44c;"/>
+</node>
+</node>
+</node>
+</map>
